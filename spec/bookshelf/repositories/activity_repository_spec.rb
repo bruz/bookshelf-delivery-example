242fa32ec74f5186243c2d8fdbb8a69093a71d42
Bruz Marzolf <bruz@bruzilla.com>
2016-09-19 21:10:41 -0700

    Correct issues raised by updated Rubocop

--- spec/bookshelf/repositories/activity_repository_spec.rb
+++ spec/bookshelf/repositories/activity_repository_spec.rb
@@ -13,14 +13,14 @@ describe ActivityRepository do
   before do
     ActivityRepository.clear
 
-    @activity_1 = create_activity(Time.now - 1)
-    @activity_2 = create_activity(Time.now)
+    @activity1 = create_activity(Time.now - 1)
+    @activity2 = create_activity(Time.now)
   end
 
   describe '#newest_to_oldest' do
     it 'provides the activities in the correct order' do
       result = ActivityRepository.newest_to_oldest
-      result.to_a.must_equal [@activity_2, @activity_1]
+      result.to_a.must_equal [@activity2, @activity1]
     end
   end
 end
